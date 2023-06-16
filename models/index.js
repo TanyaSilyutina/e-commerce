3e187597b7f990a29be381661416f8cef77cfb9c
Tanya Silyutina <48433669+TanyaSilyutina@users.noreply.github.com>
2023-06-16 00:25:51 -0700

    fixed models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,3 @@
-//replace the modes/index.js file with this file
-
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -8,7 +6,15 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
